743586205f12396b3c9c2e1dd49e66db3a778406
Dawid Pszczółkowski <pszczol.91@gmail.com>
2012-12-02 22:57:58 +0100

    Usuwanie

--- sklep/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Sklep::Application.config.secret_token = 'a43f6392b02b2883ccce5679803b387c21fb35f2422c0b292d0f9eee0a0937b197c6f0447b46eb63ebc42585894a472780c968d5a57bf7268b6b63b65ca6aa33'
