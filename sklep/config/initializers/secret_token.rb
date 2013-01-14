2401508fddbd0ef36ad897718e4242bfd34c03a9
Dawid Pszczółkowski <pszczol.91@gmail.com>
2013-01-14 12:17:03 +0100

    Sklep z Zamawianiem

---
+++ sklep/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '18ffcdf8e566d2c1fe39ce8c93ed1431400a3785d65fea46f651f69cc874d16cf239500af6bbe95a78ea9ab1bb992df39ace0e7a6f7c2fdedd1afdea44d2db7f'
